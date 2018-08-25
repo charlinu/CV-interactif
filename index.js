6517f77b693bd99e1f6e266a8f8003bea1f26e42
charlinu <charlinenekogirl@gmail.com>
2018-08-25 16:21:32 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+new SmoothScroll('a[href*="#"]');
+// ça marche pas comme ça devrait. 
+// Un offset est requis, sinon n'importe quel id semble
+// avoir une position calculée vers la section compétences
+new Waypoint({
+    element: document.getElementById('who'),
+    handler: function(direction) {
+        const arrows = document.getElementsByClassName('arrow');
+        for(const arrow of arrows) {
+            if(direction === 'down') {
+                arrow.classList.add('d-none');
+            }
+            else {
+                arrow.classList.remove('d-none');
+            }
+        }
+    },
+    offset: 800
+});
+
+// new Waypoint{
+//     element: 
+// }
